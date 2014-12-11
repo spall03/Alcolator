0edbd2ca78d9792ebfb63281450036d13e7575c9
Steve Palley <spall03@gmail.com>
2014-12-11 14:06:08 -0800

    Initial Commit

---
+++ Alcolator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alcolator
+//
+//  Created by Stephen Palley on 12/11/14.
+//  Copyright (c) 2014 Steve Palley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Stephen Palley on 12/11/14.
+//  Copyright (c) 2014 Steve Palley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
